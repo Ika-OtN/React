5de6e33858fe3ca135257292fc32b3de12d0b7fd
Ika-OtN <schneiderwaldemar@outlook.de>
2022-02-27 22:20:42 +0100

    change pizza

--- vite-react-tailwindcss/src/App.jsx
+++ vite-react-tailwindcss/src/App.jsx
@@ -1,13 +1,36 @@
-import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1 className="text-2xl text-center font-bold underline h-12">
-        Hello vite^^
-      </h1>
+    // mobile first Ansatz
+    <div class="flex flex-col items-center md:flex-row md:justify-center">
+
+      <div class="shadow-md m-4 max-w-lg hover:shadow-lg xl:grid grid-cols-5 xl:max-w-2xl">
+        
+        <img src="./images/image1.jpg" alt='' class="col-span-3 w-full h-48 object-cover" />
+        
+        <div class="p-6 col-span-2">
+          <h3 class="text-blue-400">Pizza, Rezepte</h3>
+          <h2 class="text-2xl font-bold pt-2 pb-1">Leckere Pizza</h2>
+          <p class="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, atque!
+          </p>
+        </div>
+      </div>
+
+      <div class="shadow-md m-4 max-w-lg hover:shadow-lg xl:grid grid-cols-5 xl:max-w-2xl">
+        
+        <img src="./images/image2.jpg" alt='' class="col-span-3 w-full h-48 object-cover" />
+        
+        <div class="p-6 col-span-2">
+          <h3 class="text-blue-400">Pizza, Rezepte</h3>
+          <h2 class="text-2xl font-bold pt-2 pb-1">Leckere Pizza</h2>
+          <p class="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, atque!
+          </p>
+        </div>
+      </div>
+
     </div>
   )
 }
